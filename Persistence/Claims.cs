07d128a7cc2909ce0c3a2eeda4fca1792dfe4723
Keith Holloway <keith@eastfive.com>
2017-02-13 10:37:41 -0600

    Added Claims post via the SessionServer business layer. Added Claims.cs

---
+++ Persistence/Claims.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using BlackBarLabs.Collections.Async;
+using BlackBarLabs.Persistence.Azure;
+using BlackBarLabs.Persistence.Azure.StorageTables;
+using BlackBarLabs.Extensions;
+using BlackBarLabs.Persistence;
+using BlackBarLabs.Persistence.Azure.Extensions;
+using System.Linq;
+using BlackBarLabs.Linq;
+using BlackBarLabs;
+using System.Collections.Generic;
+using System.Net.Http;
+using EastFive.Security.SessionServer.Persistence.Documents;
+
+namespace EastFive.Security.SessionServer.Persistence
+{
+    public class Claims
+    {
+        private AzureStorageRepository repository;
+        public Claims(AzureStorageRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task<TResult> CreateOrUpdateAsync<TResult>(Guid actorId, Guid claimId, string type, string value,
+              Func<TResult> onSuccess,
+              Func<TResult> onFailure,
+              Func<TResult> onActorNotFound)
+        {
+            return await await repository.FindByIdAsync(actorId,
+                (ActorMappingsDocument document) =>
+                {
+                    var claimDoc = new ClaimDocument()
+                    {
+                        ClaimId = claimId,
+                        Issuer = actorId.ToString("N"), //TODO - Is this is the correct issuer data???
+                        Type = type,
+                        Value = value
+                    };
+                    var result = document.AddOrUpdateClaimsAsync(claimDoc, repository,
+                        onSuccess,
+                        onFailure);
+                    return result;
+                },
+                () => onActorNotFound().ToTask());
+        }
+    }
+}
