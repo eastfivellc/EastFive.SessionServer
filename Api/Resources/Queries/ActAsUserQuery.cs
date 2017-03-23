c5c0f34d7f38fa1e717ca0959b6575ae4d4da5e8
Keith Holloway <keith@eastfive.com>
2017-03-23 10:25:42 -0500

    Added ActAsUserController and underlying structure to allow admin to log in as another user.

---
+++ Api/Resources/Queries/ActAsUserQuery.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.Serialization;
+using BlackBarLabs.Api.Resources;
+using Newtonsoft.Json;
+
+namespace EastFive.Security.SessionServer.Api.Resources.Queries
+{
+    [DataContract]
+    public class ActAsUserQuery : BlackBarLabs.Api.ResourceQueryBase
+    {
+        [JsonProperty("actorId")]
+        public WebIdQuery ActorId { get; set; }
+
+        /// <summary>
+        /// Super admin token
+        /// </summary>
+        [JsonProperty("token")]
+        public StringQuery Token { get; set; }
+
+        [JsonProperty("redirectUri")]
+        public StringQuery RedirectUri { get; set; }
+    }
+}
