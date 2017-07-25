b1e893b433889ed4c78fcc66b8b6f02db443c7cb
Josh <josh@eastfive.com>
2017-07-25 07:16:59 -0500

    added ping files

---
+++ Api/Controllers/PingResponseController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using BlackBarLabs.Api.Controllers;
+
+using BlackBarLabs;
+using BlackBarLabs.Api;
+using EastFive.Api.Services;
+using System.Xml;
+using BlackBarLabs.Extensions;
+using EastFive.IdentityServer;
+using System.Text;
+using System.IO;
+using System.Net.Http.Headers;
+
+namespace EastFive.Security.SessionServer.Api.Controllers
+{
+    public class PingRequest
+    {
+        public string tokenid { get; set; }
+
+        public string agentid { get; set; }
+    }
+
+    // aadb2c/SAMLRedirect?tokenid=ID7ee36a406286079a7237b23dd7647d95b8d42ddbcde4fbe8030000015d5790a407&agentid=e924bba8
+    [RoutePrefix("aadb2c")]
+    public class PingResponseController : BaseController
+    {
+        public Task<IHttpActionResult> Get([FromUri]PingRequest query)
+        {
+            return ParsePingResponseAsync(query.tokenid, query.agentid);
+        }
+
+        private async Task<IHttpActionResult> ParsePingResponseAsync(string tokenId, string agentId)
+        {
+            if (String.IsNullOrWhiteSpace(tokenId))
+                return this.Request.CreateResponse(HttpStatusCode.Conflict)
+                            .AddReason("SAML Response not provided in form POST")
+                            .ToActionResult();
+            var context = Request.GetSessionServerContext();
+            var response = await context.Sessions.CreateAsync(Guid.NewGuid(),
+                CredentialValidationMethodTypes.Ping,
+                tokenId + ":" + agentId,
+                (authorizationId, token, refreshToken, extraParams) =>
+                {
+                    var config = Library.configurationManager;
+                    var redirectResponse = config.GetRedirectUri<IHttpActionResult>(CredentialValidationMethodTypes.SAML,
+                        authorizationId, token, refreshToken, extraParams,
+                        (redirectUrl) => Redirect(redirectUrl),
+                        (paramName, why) => Request.CreateResponse(HttpStatusCode.BadRequest).AddReason(why).ToActionResult(),
+                        (why) => Request.CreateResponse(HttpStatusCode.BadRequest).AddReason(why).ToActionResult());
+                    return redirectResponse;
+                },
+                () => this.Request.CreateResponse(HttpStatusCode.Conflict)
+                            .AddReason("Already exists")
+                            .ToActionResult(),
+                (why) => this.Request.CreateResponse(HttpStatusCode.BadRequest)
+                            .AddReason($"Invalid token:{why}")
+                            .ToActionResult(),
+                () => this.Request.CreateResponse(HttpStatusCode.Conflict)
+                            .AddReason("Token does not work in this system")
+                            .ToActionResult(),
+                () => this.Request.CreateResponse(HttpStatusCode.Conflict)
+                            .AddReason("Token is not connected to a user in this system")
+                            .ToActionResult(),
+                (why) => this.Request.CreateResponse(HttpStatusCode.BadGateway)
+                            .AddReason(why)
+                            .ToActionResult(),
+                (why) => this.Request.CreateResponse(HttpStatusCode.ServiceUnavailable)
+                            .AddReason(why)
+                            .ToActionResult());
+
+            return response;
+        }
+
+
+    }
+}
---
+++ CredentialProviders/PingProvider.cs
@@ -0,0 +1,80 @@
+﻿using BlackBarLabs.Extensions;
+using EastFive.Security.CredentialProvider;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using EastFive.Security.SessionServer.Persistence;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net;
+
+namespace EastFive.Security.SessionServer.CredentialProvider.Ping
+{
+    public class PingProvider : IProvideCredentials
+    {
+        private DataContext dataContext;
+
+        public PingProvider(Persistence.DataContext context)
+        {
+            this.dataContext = context;
+        }
+        
+        
+        private static string GetTokenServiceUrl(string pingConnectToken)
+        {
+            return $"https://sso.connect.pingidentity.com/sso/TXS/2.0/1/{pingConnectToken}";
+            //return "https://sso.connect.pingidentity.com/sso/TXS/2.0/2/" + pingConnectToken;
+        }
+        
+        public async Task<TResult> RedeemTokenAsync<TResult>(string token,
+            Func<Guid, IDictionary<string, string>, TResult> onSuccess,
+            Func<string, TResult> invalidCredentials,
+            Func<TResult> onAuthIdNotFound,
+            Func<string, TResult> couldNotConnect)
+        {
+            var tokenSplit = token.Split(new char[] { ':' });
+            var tokenId = tokenSplit[0];
+            var agentId = tokenSplit[1];
+            using (var httpClient = new HttpClient())
+            {
+                var restApiKey = "tJsb,j1m"; // TODO: Read this and next from config
+                var restAuthUsername = "c0b42256-3d73-43a9-9a3b-d5b1d47aa08a";
+                var credentials = Encoding.ASCII.GetBytes($"{restAuthUsername}:{restApiKey}");
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(credentials));
+
+                var tokenUrl = GetTokenServiceUrl(tokenId);
+                var request = new HttpRequestMessage(
+                    new HttpMethod("GET"), tokenUrl);
+
+                request.Headers.Add("Cookie", "agentid=" + agentId);
+                //request.Content.Headers.ContentType = new MediaTypeHeaderValue("text/plain;charset=utf-8");
+
+                var response =
+                    await httpClient.SendAsync(request);
+                var content = await response.Content.ReadAsStringAsync();
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    dynamic stuff = Newtonsoft.Json.JsonConvert.DeserializeObject(content);
+                    string subject = stuff.subject;
+                    var hash = SHA512.Create().ComputeHash(System.Text.Encoding.UTF8.GetBytes(subject));
+                    var id = new Guid(hash.Take(16).ToArray());
+
+                    return onSuccess(id, new Dictionary<string, string>()); // TODO: Read extra params
+                }
+                else
+                {
+                    content.GetType();
+                    return invalidCredentials(content);
+                }
+            }
+        }
+    }
+}
