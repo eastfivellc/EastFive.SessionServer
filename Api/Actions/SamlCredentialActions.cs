ebfd73426eaa91b662c911b5fbfaa6c534beae9d
Josh <josh@eastfive.com>
2017-08-09 08:55:34 -0500

    removed some magic strings

--- Api/Actions/SamlCredentialActions.cs
+++ Api/Actions/SamlCredentialActions.cs
@@ -102,17 +102,17 @@ namespace EastFive.Security.SessionServer.Api
             Guid actorPerformingId, System.Security.Claims.Claim [] claims)
         {
             var context = request.GetSessionServerContext();
-            return await await context.PasswordCredentials.GetPasswordCredentialAsync(passwordCredentialId,
-                async (passwordCredential) =>
+            return await context.PasswordCredentials.GetPasswordCredentialAsync(passwordCredentialId,
+                    actorPerformingId, claims,
+                (passwordCredential) =>
                 {
-                    if (!await Library.configurationManager.CanAdministerCredentialAsync(passwordCredential.actorId, actorPerformingId, claims))
-                        return request.CreateResponse(HttpStatusCode.NotFound);
                     var response = request.CreateResponse(HttpStatusCode.OK,
                         Convert(passwordCredential, urlHelper));
                     return response;
                 },
-                () => request.CreateResponse(HttpStatusCode.NotFound).ToTask(),
-                (why) => request.CreateResponse(HttpStatusCode.NotFound).ToTask());
+                () => request.CreateResponse(HttpStatusCode.NotFound),
+                () => request.CreateResponse(HttpStatusCode.NotFound),
+                (why) => request.CreateResponse(HttpStatusCode.NotFound));
         }
 
         private async static Task<HttpResponseMessage[]> QueryByActorId(Guid actorId,
