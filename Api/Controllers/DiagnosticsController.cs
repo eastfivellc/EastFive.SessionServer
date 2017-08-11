452a2717142dc66334e938f6c6b515d0f383e9fc
Scott Turner <turner-scott@tds.net>
2017-08-11 14:26:00 -0500

    Diagnostics controller for super admins to show all the config settings

---
+++ Api/Controllers/DiagnosticsController.cs
@@ -0,0 +1,45 @@
+﻿using BlackBarLabs.Api;
+using Newtonsoft.Json;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using BlackBarLabs.Extensions;
+
+namespace EastFive.Security.SessionServer.Api.Controllers
+{
+    public class AppSetting
+    {
+        [JsonProperty(PropertyName = "Name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "Value")]
+        public string Value { get; set; }
+    }
+
+    [RoutePrefix("aadb2c")]
+    public class DiagnosticsController : BaseController
+    {
+        [HttpGet]
+        public async Task<HttpResponseMessage> Get([FromUri]AccountLinksQuery q)
+        {
+            return await this.Request.GetActorIdClaimsAsync((actorId, claims) =>
+            {
+                return EastFive.Web.Configuration.Settings.GetGuid(
+                    EastFive.Api.Configuration.SecurityDefinitions.ActorIdSuperAdmin,
+                    (actorIdSuperAdmin) =>
+                    {
+                        if (actorIdSuperAdmin == actorId)
+                        {
+                            var settings = ConfigurationManager.AppSettings.AllKeys
+                                .Select(x => new AppSetting { Name = x, Value = ConfigurationManager.AppSettings[x] }).OrderBy(x => x.Name).ToArray();
+                            return this.Request.CreateResponse(System.Net.HttpStatusCode.OK, settings, "application/json").ToTask();
+                        }
+                        return this.Request.CreateResponse(System.Net.HttpStatusCode.InternalServerError).ToTask();
+                    },
+                    (why) => this.Request.CreateResponse(System.Net.HttpStatusCode.InternalServerError, why).ToTask());
+            });
+        }
+    }
+}
