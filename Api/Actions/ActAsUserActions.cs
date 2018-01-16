ff96ec1e3017e782c07855f34c67e193d3497fde
Josh Wingstrom <josh@eastfive.com>
2018-01-16 14:51:02 -0600

    passing token with links in AAU

--- Api/Actions/ActAsUserActions.cs
+++ Api/Actions/ActAsUserActions.cs
@@ -28,7 +28,7 @@ namespace EastFive.Security.SessionServer.Api
         {
             return await query.ParseAsync(request,
                 q => QueryByActorAsync(q.RedirectUri.ParamValue(), q.ActorId.ParamSingle(), request, urlHelper),
-                // q => QueryByTokenAsync(q.RedirectUri.ParamValue(), q.Token.ParamValue(), request, urlHelper, redirect),
+                q => QueryByActorAndTokenAsync(q.RedirectUri.ParamValue(), q.ActorId.ParamSingle(), q.Token.ParamValue(), request, urlHelper),
                 q => QueryAllActorsAsync(q.RedirectUri.ParamValue(), q.Token.ParamValue(), request, urlHelper),
                 q => QueryAllActorsAsync(q.RedirectUri.ParamValue(), request, urlHelper));
         }
@@ -37,17 +37,18 @@ namespace EastFive.Security.SessionServer.Api
             HttpRequestMessage request, UrlHelper url)
         {
             return await request.GetActorIdClaimsAsync(
-                (actorId, claims) => QueryAllActorsAsync(redirectUri, actorId, claims, request, url));
+                (actorId, claims) => QueryAllActorsAsync(redirectUri, actorId, claims, request.Headers.Authorization.ToString(), request, url));
         }
 
         public static async Task<HttpResponseMessage> QueryAllActorsAsync(string redirectUri, string token,
             HttpRequestMessage request, UrlHelper url)
         {
             return await request.GetActorIdClaimsFromTokenAsync(token,
-                (actorId, claims) => QueryAllActorsAsync(redirectUri, actorId, claims, request, url));
+                (actorId, claims) => QueryAllActorsAsync(redirectUri, actorId, claims, token, request, url));
         }
 
-        public static async Task<HttpResponseMessage> QueryAllActorsAsync(string redirectUri, Guid actorPerforming, System.Security.Claims.Claim [] claims,
+        public static async Task<HttpResponseMessage> QueryAllActorsAsync(string redirectUri, 
+                Guid actorPerforming, System.Security.Claims.Claim [] claims, string token,
             HttpRequestMessage request, UrlHelper url)
         {
             var context = request.GetSessionServerContext();
@@ -67,6 +68,7 @@ namespace EastFive.Security.SessionServer.Api
                                         .NullToEmpty()
                                         .Append("RedirectUri".PairWithValue(redirectUri))
                                         .Append("ActorId".PairWithValue(info.ActorId.ToString()))
+                                        .Append("Token".PairWithValue(token))
                                         .Aggregate(
                                             url.GetLocation<Controllers.ActAsUserController>(),
                                             (baseUrl, param) => baseUrl.AddParameter(param.Key, param.Value),
@@ -101,103 +103,52 @@ namespace EastFive.Security.SessionServer.Api
             HttpRequestMessage request, UrlHelper url)
         {
             return await request.GetActorIdClaimsAsync(
-                async (actorPerforming, claims) =>
-                {
-                    if (!await Library.configurationManager.CanAdministerCredentialAsync(actorId, actorPerforming, claims))
-                        return request.CreateResponse(HttpStatusCode.Unauthorized, $"Actor [{actorPerforming}] cannot administer [{actorId}]");
-
-                    var context = request.GetSessionServerContext();
-                    var authenticationRequestId = Guid.NewGuid();
-                    var redirectUrl = new Uri(redirectString);
-                    var method = ServiceConfiguration.credentialProviders.First().Value.Method;
-                    return await await context.Sessions.CreateLoginAsync(authenticationRequestId, actorId,
-                            method, redirectUrl,
-                        async (session) =>
-                        {
-                            var config = Library.configurationManager;
-                            var redirectResponse = await config.GetRedirectUriAsync(context,
-                                    method, AuthenticationActions.signin,
-                                    authenticationRequestId, actorId, session.token, session.refreshToken, session.extraParams,
-                                    redirectUrl,
-                                (redirectUrlSelected) => request.CreateRedirectResponse(redirectUrlSelected),
-                                (paramName, why) =>
-                                {
-                                    var message = $"Invalid parameter while completing login: {paramName} - {why}";
-                                    return request.CreateResponse(HttpStatusCode.BadRequest, message).AddReason(why);
-                                },
-                                (why) =>
-                                {
-                                    var message = $"General failure while completing login: {why}";
-                                    return request.CreateResponse(HttpStatusCode.BadRequest, message)
-                                        .AddReason(why);
-                                });
-                            return redirectResponse;
-                        },
-                        "Guid not unique for creation of authentication request id".AsFunctionException<Task<HttpResponseMessage>>(),
-                        (why) => request.CreateResponseConfiguration("", why).ToTask());
-                });
+                (actorPerforming, claims) => QueryByActorAsync(redirectString, actorId, actorPerforming, claims, request, url));
         }
 
-        //private static async Task<HttpResponseMessage> QueryByTokenAndActorIdAsync(string redirectBase, string token,
-        //    HttpRequestMessage request, UrlHelper url,
-        //    Func<string, RedirectResult> redirect)
-        //{
-        //    var context = request.GetSessionServerContext();
-        //    var result = await await context.Sessions.CreateLoginAsync(Guid.NewGuid(),
-        //        CredentialValidationMethodTypes.Password, "", loginId, Guid.NewGuid(),
-        //        new Dictionary<string, string>(),
-        //        async (authorizationId, tken, refreshToken, extraParams) =>
-        //        {
-        //            return await Library.configurationManager.GetRedirectUriAsync(context, CredentialValidationMethodTypes.Password,
-        //                AuthenticationActions.signin,
-        //                loginId,
-        //                authorizationId, tken, refreshToken, extraParams, default(Uri),
-        //                (redirectUrl) =>
-        //                {
-        //                    var host = request.RequestUri.Host;
-        //                    if (Uri.TryCreate(redirectBase, UriKind.Absolute, out Uri userUrl) && userUrl.Host == "localhost")
-        //                    {
-        //                        var builder = new UriBuilder(redirectUrl)
-        //                        {
-        //                            Scheme = userUrl.Scheme,
-        //                            Host = userUrl.Host,
-        //                            Port = userUrl.Port
-        //                        };
-        //                        redirectUrl = builder.Uri;
-        //                        host = redirectUrl.Host;
-        //                    }
-        //                    var response = request.CreateHtmlResponse($"<script>window.location=\"{redirectUrl}\"</script>");
-        //                    var cookie = new System.Net.Http.Headers.CookieHeaderValue(Api.Constants.Cookies.FakingId, token)
-        //                    {
-        //                        Expires = DateTimeOffset.Now.AddDays(1),
-        //                        Domain = host,
-        //                        Path = "/"
-        //                    };
-        //                    response.Headers.AddCookies(new System.Net.Http.Headers.CookieHeaderValue[] { cookie });
-        //                    return response;
-        //                },
-        //                // TODO: Add reasons
-        //                (param, why) => request.CreateResponse(HttpStatusCode.Conflict),
-        //                (why) => request.CreateResponse(HttpStatusCode.Conflict));
-        //        },
-        //        () =>
-        //        {
-        //            // Can't happen
-        //            var response = request.CreateResponse(HttpStatusCode.Conflict);
-        //            return response.ToTask();
-        //        },
-        //        () =>
-        //        {
-        //            return request.CreateResponse(HttpStatusCode.NotFound)
-        //                .AddReason($"The provided loginId [{loginId}] did not map to a user in this system.")
-        //                .ToTask();
-        //        },
-        //        (why) =>
-        //        {
-        //            return request.CreateResponse(HttpStatusCode.ServiceUnavailable)
-        //                .AddReason(why).ToTask();
-        //        });
-        //    return result;
-        //}
+        private static async Task<HttpResponseMessage> QueryByActorAndTokenAsync(string redirectBase, Guid actorId, string token,
+            HttpRequestMessage request, UrlHelper url)
+        {
+            return await request.GetActorIdClaimsFromTokenAsync(token,
+                (actorPerforming, claims) => QueryByActorAsync(redirectBase, actorId, actorPerforming, claims, request, url));
+        }
+
+        private static async Task<HttpResponseMessage> QueryByActorAsync(string redirectString, Guid actorId,
+            Guid actorPerforming, System.Security.Claims.Claim[] claims,
+            HttpRequestMessage request, UrlHelper url)
+        {
+            if (!await Library.configurationManager.CanAdministerCredentialAsync(actorId, actorPerforming, claims))
+                return request.CreateResponse(HttpStatusCode.Unauthorized, $"Actor [{actorPerforming}] cannot administer [{actorId}]");
+
+            var context = request.GetSessionServerContext();
+            var authenticationRequestId = Guid.NewGuid();
+            var redirectUrl = new Uri(redirectString);
+            var method = ServiceConfiguration.credentialProviders.First().Value.Method;
+            return await await context.Sessions.CreateLoginAsync(authenticationRequestId, actorId,
+                    method, redirectUrl,
+                async (session) =>
+                {
+                    var config = Library.configurationManager;
+                    var redirectResponse = await config.GetRedirectUriAsync(context,
+                            method, AuthenticationActions.signin,
+                            authenticationRequestId, actorId, session.token, session.refreshToken, session.extraParams,
+                            redirectUrl,
+                        (redirectUrlSelected) => request.CreateRedirectResponse(redirectUrlSelected),
+                        (paramName, why) =>
+                        {
+                            var message = $"Invalid parameter while completing login: {paramName} - {why}";
+                            return request.CreateResponse(HttpStatusCode.BadRequest, message).AddReason(why);
+                        },
+                        (why) =>
+                        {
+                            var message = $"General failure while completing login: {why}";
+                            return request.CreateResponse(HttpStatusCode.BadRequest, message)
+                                .AddReason(why);
+                        });
+                    return redirectResponse;
+                },
+                "Guid not unique for creation of authentication request id".AsFunctionException<Task<HttpResponseMessage>>(),
+                (why) => request.CreateResponseConfiguration("", why).ToTask());
+        }
     }
 }
